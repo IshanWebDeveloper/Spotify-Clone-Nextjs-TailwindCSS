e6e60fe751ce06439519b36e78f910aef02ea912
IshanWebDeveloper <ishananaguru@gmail.com>
2021-12-26 01:36:26 +0530

    intial build

---
+++ atoms/playlistAtoms.js
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: "5hlCT3pQ6RRXc2zWaYLTut"
+})
+
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null
+})
---
+++ components/Songs.jsx
@@ -0,0 +1,16 @@
+import { useRecoilValue } from 'recoil';
+import { playlistState } from '../atoms/playlistAtoms';
+import Song from './Song';
+
+function Songs() {
+  const playlist = useRecoilValue(playlistState);
+  return (
+    <div className="text-white p-3">
+      {playlist?.tracks.items.map((track, i) => (
+        <Song key={track.track.id} track={track} order={i} />
+      ))}
+    </div>
+  );
+}
+
+export default Songs;
